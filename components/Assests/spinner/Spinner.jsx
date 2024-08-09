5dc01e9e61f564e5f35a193021b7145c7e8edee3
donpushme <midabrey@gmail.com>
2024-08-10 04:44:09 +0900

    fix:nftSnapshot

--- components/Assests/spinner/Spinner.jsx
+++ components/Assests/spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Spinner() {
   return (
     <div className="relative w-4 aspect-square z-50">
-      <Image src="/spinner/spinner_round.gif" fill alt="" />
+      <Image src="/spinner/spinner.gif" fill alt="" />
     </div>
   );
 }
